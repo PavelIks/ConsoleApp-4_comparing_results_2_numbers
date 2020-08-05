700040c50dd8f94ea416343de2804965bcc3c3c1
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-05 14:57:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FourComparisonResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 1-е число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите 2-е число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("\n" + a + " > " + b + ":  "); Console.WriteLine(a > b);
+            Console.Write(a + " < " + b + ":  "); Console.WriteLine(a < b);
+            Console.Write(a + " == " + b + ": "); Console.WriteLine(a == b);
+            Console.Write(a + " != " + b + ": "); Console.WriteLine(a != b);
+            Console.ReadKey();
+        }
+    }
+}
